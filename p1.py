ed58a2e1eb78f7c96bfceb001050cd0337401ee1
spero99 <sperako948@gmail.com>
2018-03-27 20:48:12 +0300

    commit.

--- p1.py
+++ p1.py
@@ -1,8 +1,6 @@
 #using python 3.6
 #p17095-applied algebra program no.1
 
-
-
 s = raw_input("dwse meta8esh\n")
 given=[]
 given.append(map(int, s.split()))
@@ -14,16 +12,19 @@ print("to mhkos ths meta8eshs einai:"+str(length))
 meta8esh=[]
 for i in range(length):
     meta8esh.append([i+1,0])
-print(meta8esh)
+
 for j in range(length):
     help1=given[0][j]
-    meta8esh[j][1].append(help1)
+    meta8esh[j][1]=help1
 print (meta8esh)
-#def BresAntistrofh(met8esh):
-'''antistrofh=[]
+
+antistrofh=[]
+
 for i in range(length):
-    help2=meta8esh[i][1]
-    antistrofh[help2].append(i)
-print (str(antistrofh.join(" ")))
-#print ("h antistrofh einai:"+BresAntistrofh(meta8esh))
-'''
+    antistrofh.append(0)
+
+for i in range(length):
+    help2=int(meta8esh[i][1])
+    antistrofh[help2-1]=i+1
+
+print ("h antistrofh einai:"+str(antistrofh))
---
+++ p3.py
@@ -0,0 +1,11 @@
+#using python 3.6
+#p17095-applied algebra program no.3
+
+
+print ("dwse mhtres ABCDE mia ana grammh(px. 3x3)")
+mhtres=[]
+numbers=[]
+for i in range(5):
+    given=raw_input("\n")
+    numbers.append(map(int, given.split("x")))
+print(numbers)
