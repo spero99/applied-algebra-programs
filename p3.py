2fa637f3d8125ee0d681cf348202d49cb0e9fef9
spero99 <sperako948@gmail.com>
2018-03-28 22:09:50 +0300

    commit

--- p3.py
+++ p3.py
@@ -6,16 +6,18 @@ print ("dwse mhtres ABCDE mia ana grammh(px. 3x3)")
 mhtres=[]
 numbers=[]
 for i in range(5):
-    given=raw_input("\n")
+    given=raw_input("")
     mhtres.append(map(int, given.split("x")))
 print("oi mhtres einai :"+str(mhtres))                                          #testline
 
-
+count=0
 def synarthsh(nunbers):
     max=0
     n=0
     m=0
     count=0
+    help1=0
+    help2=0
     for i in range(len(numbers)):
         for j in range(len(numbers)):
              if numbers[i][1]==numbers[j][0] and numbers[j][0]>max:
@@ -29,3 +31,8 @@ def synarthsh(nunbers):
     numbers.append([n,m])
     count=(n*max*m)+count
     return numbers,count
+
+while len(numbers)!=1:
+    synarthsh(numbers)
+print("h mhtra einai:"+ str(numbers) )
+print("oi pol/smoi pou xreiasthkan einai :"+str(count))
