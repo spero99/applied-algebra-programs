0919281ed8ab30d0b49e8aa9558e8100b6f4f119
spero99 <sperako948@gmail.com>
2018-03-28 20:46:04 +0300

    commit

--- p3.py
+++ p3.py
@@ -7,5 +7,25 @@ mhtres=[]
 numbers=[]
 for i in range(5):
     given=raw_input("\n")
-    numbers.append(map(int, given.split("x")))
-print(numbers)
+    mhtres.append(map(int, given.split("x")))
+print("oi mhtres einai :"+str(mhtres))                                          #testline
+
+
+def synarthsh(nunbers):
+    max=0
+    n=0
+    m=0
+    count=0
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+             if numbers[i][1]==numbers[j][0] and numbers[j][0]>max:
+                max=numbers[j][0]
+                n=numbers[i][0]
+                m=numbers[j][1]
+                help1=i
+                help2=j
+    numbers.remove(numbers[help1])
+    numbers.remove(numbers[help2])
+    numbers.append([n,m])
+    count=(n*max*m)+count
+    return numbers,count
